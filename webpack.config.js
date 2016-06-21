b1efdb84dba98bfd7bd6c8e2ce93e120fdbc887c
michelleheh <xiaoyu.he22@gmail.com>
2016-06-21 10:22:28 -0700

    Create webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
